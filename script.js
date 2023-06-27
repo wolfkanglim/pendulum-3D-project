9fe37dc757094b1c9e83d6882e409c063e1ed873
truckDriverDev <hungrywolfkang@gmail.com>
2023-06-26 22:57:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,177 @@
-import * as THREE from '../modules/three.module.js';
-import { OrbitControls } from '../modules/OrbitControls.js';
+import * as THREE from './js/three.module.js';
+import {OrbitControls} from './js/OrbitControls.js';
 
 const canvas = document.getElementById('canvas1');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-//variables
+const toggles = {
+     sound: document.getElementById('sound-toggle')
+}
+
+// 21 colors
+const colors = [
+     "#D0E7F5",
+     "#D9E7F4",
+     "#D6E3F4",
+     "#BCDFF5",
+     "#B7D9F4",
+     "#C3D4F0",
+     "#9DC1F3",
+     "#9AA9F4",
+     "#8D83EF",
+     "#AE69F0",
+     "#D46FF1",
+     "#DB5AE7",
+     "#D911DA",
+     "#D601CB",
+     "#E713BF",
+     "#F24CAE",
+     "#FB79AB",
+     "#FFB6C1",
+     "#FED2CF",
+     "#FDDFD5",
+     "#FEDCD1"
+];
+
+let soundEnabled = false;
+let pulseEnabled = true;
+
+const handleSoundToggle = (enabled = !soundEnabled) => {
+     soundEnabled = enabled;
+     toggles.sound.dataset.toggled = enabled;
+}
+
+document.onvisibilitychange = () => {
+     handleSoundToggle(false);
+}
+
+document.onclick = () => {
+     handleSoundToggle();
+}
+
+const getFileName = (index) => {
+     return `key-${index}`;
+}
+
+const getURL = (index) => {
+     return `./audios/vibraphone/${getFileName(index)}.mp3`;
+}
+
+const keys = colors.map((color, index) => {
+     const audio = new Audio(getURL(index));
+     audio.currentTime = 0;
+     audio.volume = 0.2;
+     return audio;
+})
+
+const playKey = (index) => {
+     keys[index].currentTime = 0;
+     keys[index].play();
+}
+
+////////// THREE 
+
+///// variables
 let scene, camera, renderer, orbitControls;
+let cameraFront, insetWidth, insetHeight;
 
 const textureLoader = new THREE.TextureLoader();
+const space = textureLoader.load('./assets/galaxy1.jpg');
+// stone textures   
+const stoneTextureColor = textureLoader.load('./textures/wall_stone/Wall_Stone_022_basecolor.jpg');
+const stoneTextureRoughness = textureLoader.load('./textures/wall_stone/Wall_Stone_022_roughness.jpg');
+const stoneTextureNormal = textureLoader.load('./textures/wall_stone/Wall_Stone_022_normal.jpg'); 
+
+//metal textures
+const metalTextureColor = textureLoader.load('./textures/metal/Metal043A_1K_Color.jpg');
+const metalTextureDisplacement = textureLoader.load('./textures/metal/Metal043A_1K_Displacement.jpg');
+const metalTextureRoughness = textureLoader.load('./textures/metal/Metal043A_1K_Roughness.jpg');
+const metalTextureNormal = textureLoader.load('./textures/metal/Metal043A_1K_NormalGL.jpg');
+
 const color = new THREE.Color();
 const groupRight = new THREE.Object3D();
 const groupLeft = new THREE.Object3D();
 
-///// for collision detection
-let cubeBB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
-let ballBB;
+let rightCubes = [];
+let leftCubes = [];
 
 initThree();
-createLights();
 createGround();
+createLights();
 createBar();
-createXylophone();
+createSpace();
+createVibraphone();
+cameraMove();
 
 function initThree(){
      scene = new THREE.Scene();
      scene.background = new THREE.Color(0x111111);
-     scene.fog = new THREE.Fog(0x333333, 1, 180);
+     scene.background = space;
+     scene.fog = new THREE.Fog(0x555555, 1, 580);
      camera = new THREE.PerspectiveCamera(
-          75,
+          65,
           window.innerWidth/window.innerHeight,
           0.1,
-          1000
+          10000
      )
-     camera.position.set(0, 1, 12);
-     camera.lookAt(0, 0, 0);
+     camera.position.set(0, 21, 150);
+     camera.lookAt(0, 40, 0);
+     
+     //cameraFront
+     insetWidth = window.innerHeight * 0.35;
+     insetHeight = window.innerHeight * 0.35;
+     
+     cameraFront = new THREE.PerspectiveCamera( 65, insetWidth / insetHeight,
+     1, 500)
+     cameraFront.position.set(0, 20, 70);
+     cameraFront.lookAt(0, 25, 0);
+     cameraFront.name = 'frontCamera';     
+
      renderer = new THREE.WebGLRenderer({antialias: true, 
      canvas: canvas});
      renderer.setPixelRatio(window.devicePixelRatio);
      renderer.setSize(window.innerWidth, window.innerHeight);
-     document.body.appendChild(renderer.domElement);
      renderer.shadowMap.enabled = true;
 
      orbitControls = new OrbitControls(camera, renderer.domElement);
      orbitControls.enableDamping = true;
-     orbitControls.dampingFactor = 0.005;
+     orbitControls.dampingFactor = 0.04;  
+}
 
-     const axesHelper = new THREE.AxesHelper(5);
-     scene.add(axesHelper);
+//camera controls
+function cameraMove(){
+     let tl = gsap.timeline({repeat: 2, repeatDelay: 3});
+     tl.to(camera.position, {z: 50, duration:12});
+     tl.to(camera.position, {z: 30, duration:6});
+     tl.to(camera.position, {x: -30, duration: 8});
+     tl.to(camera.position, {y: 60, duration: 8});
+     tl.to(camera.position, {x: 38, duration:4});
+     tl.to(camera.position, {x: 1, duration:2});
+     tl.to(camera.position, {y: 0 , duration: 6});
+     tl.to(camera.position, {y: 30 , duration: 12});
+     tl.to(camera.positoin, {y: 10 * 0 , duration: 6});
+     tl.to(camera.position, {z: 28, duration:6}); 
+     tl.to(camera.position, {x: 10, duration:4}); 
+     tl.to(camera.rotation, {z: -Math.PI * 2, duration:6}); 
+     tl.to(camera.position, {x: 15, duration:8});  
+     tl.to(camera.position, {z: 30, duration:4});  
+     tl.to(camera.position, {y: 50, duration:6});  
+     tl.to(camera.position, {z: 20, duration:8});  
+     tl.to(camera.position, {s: 0, duration:4});  
+     tl.to(camera.position, {z: 50, duration:6});
 }
 
 function createLights(){
-     const ambientLight = new THREE.AmbientLight(0x333333, 0.8);
+     const ambientLight = new THREE.AmbientLight(0xaaaaaa, 0.51);
      scene.add(ambientLight);
-     const dirLight = new THREE.DirectionalLight(0xffffff, 1);
-     dirLight.position.set(5, 30, -5);
+     const dirLight = new THREE.DirectionalLight(0xffffff, 0.5);
+     dirLight.position.set(5, 50, 15);
      scene.add(dirLight);
      dirLight.castShadow = true;
      dirLight.shadow.mapSize.width = 1024;
      dirLight.shadow.mapSize.height = 1024;
-     const d = 20;
+     const d = 150;
      dirLight.shadow.camera.top = d;
      dirLight.shadow.camera.right = d;
      dirLight.shadow.camera.bottom = -d;
@@ -67,38 +179,50 @@ function createLights(){
      dirLight.shadow.camera.near = 0.1;
      dirLight.shadow.camera.far = 100;
 
-     const pointLight = new THREE.PointLight(0xffffff, 0.6);
-     pointLight.position.set(0, 8, 5);
+     const pointLight = new THREE.PointLight(0xffffff, 0.8);
+     pointLight.position.set(0, 20, 25);
      scene.add(pointLight);
 }
 
-function createGround(){
-     const stoneTextureColor = textureLoader.load('./texture/Wall_Stone_022_SD/Wall_Stone_022_basecolor.jpg');
-     const stoneTextureRoughness = textureLoader.load('./texture/Wall_Stone_022_SD/Wall_Stone_022_roughness.jpg');
-     const stoneTextureNormal = textureLoader.load('./texture/Wall_Stone_022_SD/Wall_Stone_022_normal.jpg');
+//create background sphere
+function createSpace(){
+     const geometry = new THREE.SphereGeometry(500, 50, 50);
+     const material = new THREE.MeshStandardMaterial({
+          color: 'white',
+          side: THREE.DoubleSide,
+          wireframe: true,          
+     })
+     const sphereSpace = new THREE.Mesh(geometry, material);
+     sphereSpace.rotation.x = -Math.PI/2;
+     scene.add(sphereSpace);
+}
 
-     const planeGeometry = new THREE.PlaneGeometry(50, 50);
+
+
+///// ground
+function createGround(){
+     const planeGeometry = new THREE.PlaneGeometry(150, 150);
      const planeMaterial = new THREE.MeshStandardMaterial({
-          color: 0xffffff,
-          map: stoneTextureColor,
+          color: 0x777777,
+          map: metalTextureColor,
           normalMap: stoneTextureNormal,
           normalScale: new THREE.Vector2(5, 5),
           roughness: 1,
           roughnessMap: stoneTextureRoughness
-     }) 
+     })
      const mesh = new THREE.Mesh(planeGeometry, planeMaterial);
-     mesh.rotation.x = -Math.PI/2;
-     mesh.receiveShadow = true;
-     mesh.position.set(0, -4, -10);
+     mesh.rotation.x = -Math.PI / 2;
+     mesh.receiveShadow = true,
+     mesh.position.set(0, -21, -10);
      scene.add(mesh);
 }
 
-///// instruments /////
-
-function createCubeMesh(){
+function createCube(){
      const geometry = new THREE.BoxGeometry();
      const material = new THREE.MeshPhongMaterial({
-          color: color
+          color: color,
+          transparent: true,
+          opacity: 1
      })
      const cubeMesh = new THREE.Mesh(geometry, material);
      cubeMesh.castShadow = true;
@@ -107,172 +231,202 @@ function createCubeMesh(){
      return cubeMesh;
 }
 
-
-function createXylophone(){
-     for(let i = 0; i < 12; i++){
-          const cube = createCubeMesh();
-          cube.material.color = new THREE.Color(`hsl(${i*21.24}, 100%, 50%)`);
-          cube.position.set(13.85 + (i * 0.07), 3, -i * 1.25);
-          cube.scale.set(0.25, i * 1.125 + 2, 1);
-          groupRight.add(cube);     
-          groupRight.rotation.z = Math.PI / 9;
-          
-          ///// for collision detection
-          cubeBB.setFromObject(cube);
-          cubeBB.name = `cubeBBR${i}`;
-     } 
-     
-     for(let i = 0; i < 12; i++){
-          const cube = createCubeMesh();
-          cube.material.color = new THREE.Color(`hsl(${244 - i*21.24}, 100%, 50%)`);
-          cube.position.set(-13.85 - (i * 0.07), 4, -i * 1.25);
-          cube.scale.set(0.25, (13.75 - i * 1.125) + 2, 1);
-          groupLeft.add(cube);  
-          groupLeft.rotation.z = -Math.PI/9;  
-          
-          cubeBB.setFromObject(cube);
-          cubeBB.name = `cubeBBL${i}`;          
-          //console.log(cubeBB);
-     } 
-     scene.add(groupRight);
-     scene.add(groupLeft);
-};
-
+///// create Bar Beam Stone
 function createBar(){
-     const bar = createCubeMesh();
-     bar.position.set(0, 12.5, -8);
-     bar.scale.set(1, 1, 20);
+     const bar = createCube();
+     bar.position.set(0, 32, 0);
+     bar.scale.set(1, 1, 54);
      scene.add(bar);
 
-     const beam = createCubeMesh();
-     beam.position.set(0, 0, -17.5);
-     beam.scale.set(2, 25.5, 2);
+     const beam = createCube();
+     beam.position.set(0, 0, -18);
+     beam.scale.set(2, 65, 2);
      scene.add(beam);
 
-     const stone = createCubeMesh();
-     stone.position.set(0, -4, -18);
-     stone.scale.set(8, 2, 4);
-     stone.material.color = 0x222222;
+     const stone = createCube();
+     stone.position.set(0, -18, -18);
+     stone.scale.set(18, 8, 10);
+     stone.material.color = 0x333333;
+     stone.material.map = stoneTextureColor;
      scene.add(stone);
-};
+}
 
+///// Vibraphone right/left
+function createVibraphone(){
+     for(let i = 0; i < 21; i++){
+          const cube = createCube();
+          cube.material.color = new THREE.Color(colors[i]);
+          cube.scale.set(1.5, i * 0.5 + 5, 0.5);
+          cube.position.set(6 + i * 2 , 16 + (i * 0.5), i * 0.18);
+          rightCubes.push(cube);
+          groupRight.add(cube);
+          groupRight.rotation.y = Math.PI / 2;
+          groupRight.position.set(26, 0, 30);
+     }
+     scene.add(groupRight);
 
+     for(let i = 0; i < 21; i++){
+          const cube = createCube();
+          cube.material.color = new THREE.Color(colors[i]);
+          cube.scale.set(1.5, i * 0.5 + 5, 0.5);
+          cube.position.set(-6 - (i * 2.02), 16 + (i * 0.5), i * 0.18);
+          leftCubes.push(cube);
+          groupLeft.add(cube);
+          groupLeft.rotation.y = -Math.PI / 2;
+          groupLeft.position.set(-26, 0, 30);
+     }
+     scene.add(groupLeft);
+}
 
- 
-///////////////////
+///// pendulum string mesh / ball mesh
 
+let ballRadius = 1.05;
+let stringLength = 31;
+let stringWidth = 0.05;
+const maxPendulum = 21;
 
 function createStringMesh(){
-     const geometry = new THREE.CylinderGeometry(0.05, 0.05, 12);
+     const geometry = new THREE.CylinderGeometry(stringWidth, stringWidth, stringLength);
      const material = new THREE.MeshStandardMaterial({
           color: 0xeeffee,
+
      })
      const string = new THREE.Mesh(geometry, material);
+     string.position.set(0, 0, 0);
      string.castShadow = true;
      string.receiveShadow = true;
      scene.add(string);
      return string;
-};
+}
 
 function createBallMesh(){
-     const ballTextureColor = textureLoader.load('./textures/Metal043A_1K-JPG/Metal043A_1K_Color.jpg');
-     const ballTextureRoughness = textureLoader.load('./textures/Metal043A_1K-JPG/Metal043A_1K_Roughness.jpg');
-     const geometry = new THREE.SphereGeometry(0.56);
+     const geometry = new THREE.SphereGeometry(ballRadius, 30);
      const material = new THREE.MeshStandardMaterial({
-          //color: 0x00ff00,
-          map: ballTextureColor,
+          map: metalTextureColor,
           roughness: 1,
-          roughnessMap: ballTextureRoughness,
+          roughnessMap: metalTextureRoughness,
           metalness: 0.2
      })
      const ball = new THREE.Mesh(geometry, material);
      ball.castShadow = true;
      ball.receiveShadow = true;
-     scene.add(ball);
+     scene.add(ball);     
      return ball;
-};
+}
 
 class Pendulum {
-     constructor(stringMesh, ballMesh, frequency, amplitude){
-          
-          this.string = stringMesh;
-          this.ball = ballMesh;
+     constructor(string, ball, frequency, amplitude){
+          this.string = string;
+          this.ball = ball;
           this.frequency = frequency;
           this.amplitude = amplitude;
      }
      update(totalTime){
           this.string.rotation.z = this.amplitude * Math.cos((this.frequency * totalTime)/1000);
           this.ball.rotation.z = this.amplitude * Math.cos((this.frequency * totalTime)/1000);
+          
      }
 };
 
 function createPendulum(origin, frequency, amplitude){
      const stringMesh = createStringMesh();
      stringMesh.position.add(origin);
-     stringMesh.translateY(12);
-     stringMesh.geometry.translate(0, -6, 0);
+     stringMesh.translateY(stringLength);
+     stringMesh.geometry.translate(0, -(stringLength * 0.5), 0);
 
      const ballMesh = createBallMesh();
      ballMesh.position.add(origin);
-     ballMesh.translateY(12);
-     ballMesh.geometry.translate(0, -12.5, 0);
+     ballMesh.translateY(stringLength);
+     ballMesh.geometry.translate(0, -stringLength, 0);
 
-     ///// for collision detection
-     ballBB = new THREE.Sphere(ballMesh.position, 0.7);
-     ballBB.name = 'ball';
-     //console.log(ballBB);
-
+    
      const pendulum = new Pendulum(stringMesh, ballMesh, frequency, amplitude);
      return pendulum;
 };
 
 const pendulums = [];
-for(let i = 0; i < 12; i++){
-     const pendulum = createPendulum(new THREE.Vector3(0, 0, -i * 1.25), 1.2 + i * 0.0125, 1.2 + i * 0.01);
+for(let i = 0; i < maxPendulum; i++){
+     const pendulum = createPendulum(new THREE.Vector3(0, 0, -i * 2 + 24), 1 + i * 0.0125, 1 + i * 0.0125);
      pendulums.push(pendulum);
 }; 
 
-/* for(let i = 0; i < 7; i++){
-     const pendulum = createPendulum(new THREE.Vector3(0, 0, 0), 1.2 + i * 0.0125, 3.14/4);
-     pendulums.push(pendulum);
-};  */
+
+
 
 let startTime = null;
-//let lastTime = null;
 
 function animate(time){
      if(startTime == null){
           startTime = time;
      }
-     //if(lastTime == null){
-          //lastTime = time;
-     //}
-     //const deltaTime = time - lastTime;
-     //lastTime = time;
+    
      const totalTime = time - startTime;
      update(totalTime);
+
+     renderer.setViewport(0, 0, window.innerWidth, window.innerHeight);// need for cameraFront
      renderer.render(scene, camera);
      window.requestAnimationFrame(animate);
+
+     //for cameraFront
+     renderer.clearDepth();
+     renderer.setScissorTest(true);
+     renderer.setScissor(window.innerWidth - insetWidth - 8,  insetHeight / 4 ,
+          insetWidth, insetHeight);
+     renderer.setViewport(window.innerWidth - insetWidth - 8,  insetHeight / 4, insetWidth, insetHeight + 16);
+     renderer.render(scene, cameraFront);
+     
+     renderer.setScissorTest(false);
 }
 
 function update(totalTime){
-          pendulums.forEach((p) => {p.update(totalTime);
           orbitControls.update();
+          //console.log(pendulums[20].ball.rotation.z);
+          pendulums.forEach((p, index) => {
+               p.update(totalTime);
+
+               //if(p.ball.rotation.z >= 1) {
+               if(p.ball.rotation.z >= (0.999 + index * 0.01245)) {
+                    if(soundEnabled){
+                         playKey(index);
+                    }
+                    if(pulseEnabled){
+                         gsap.to(rightCubes[index].position, {y: index/2 + 16 + 20, duration: 1, ease: 'power3'}) ;
+                         gsap.to(rightCubes[index].material, {opacity: 1, duration: 0.1}) ; 
+                         gsap.to(rightCubes[index].position, {y: index/2 + 16, duration: 2}) ;
+                         //gsap.to(rightCubes[index].rotation, {y: 360, duration: 2}) ;
+                         gsap.to(rightCubes[index].material, {opacity: 0.76, duration: 2}) ; 
+                    }
+                   
+               }  else if(p.ball.rotation.z <= -(0.999 + index * 0.01245)) {
+                    if(soundEnabled){
+                         playKey(index);
+                    }
+                    if(pulseEnabled){
+                         gsap.to(leftCubes[index].position, {y: 18 + (index * 1.0125)/2 + 0.9 + 20, duration: 1, ease: 'power3'}) ;
+                         gsap.to(leftCubes[index].material, {opacity: 1, duration: 0.1}) ; 
+                         gsap.to(leftCubes[index].position, {y: 15 + (index * 1.0125)/2 + 0.9, duration: 2}) ;  
+                         gsap.to(leftCubes[index].material, {opacity: 0.76, duration: 2}) ; 
+                    }
+                    
+               } 
      });
 };
 
 animate();
-//console.log(pendulums);
 
-///// collision detection but did not working why?????
-let collision = ballBB.intersectsBox(cubeBB);
-if(collision) console.log('hit sound');
 
 window.addEventListener('resize', () => {
      const aspect = window.innerWidth/window.innerHeight;
      camera.aspect = aspect;
      camera.updateProjectionMatrix();
-     camera.position.z = Math.max(8/aspect, 6);
+     //camera.position.z = Math.max(8/aspect, 6);
      camera.lookAt(0, 0, 0);
      renderer.setSize(window.innerWidth, window.innerHeight);
+
+     insetWidth = window.innerHeight * 0.35;
+     insetHeight = window.innerHeight * 0.35;
+     cameraFront.aspect = insetWidth / insetHeight;
+     cameraFront.updateProjectionMatrix();
 });
+
+
